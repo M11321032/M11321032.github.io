7d81e4911fc787deb1999c16f5911006feec4101
M11321032 <leoleo40103@gmail.com>
2026-05-07 10:54:00 +0800

    Update chatbotb.js

--- chatbotb.js
+++ chatbotb.js
@@ -3,7 +3,7 @@ class ChatBot {
     constructor() {
         // ⚠️ Important: Replace with your n8n webhook URL
         // ****************************************************************
-        this.webhookUrl = 'https://M11321032.app.n8n.cloud/webhook/chatb';
+        this.webhookUrl = 'https://leontust.app.n8n.cloud/webhook/chatb';
         // ****************************************************************        
         // Initialize session ID
         this.sessionId = this.getOrCreateSessionId();
